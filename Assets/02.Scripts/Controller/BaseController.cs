c35ef487ad58784aa0cb4cc153eddfdb049a94b0
sangweon25 <seosangweon1290@gmail.com>
2026-07-21 21:11:35 +0900

    [feat] 플레이어 이동 조작
    Input System을 활용한 플레이어 이동 및 방향 전환 조작 추가
    BaseController ,PlayerController 스크립트 생성

---
+++ Assets/02.Scripts/Controller/BaseController.cs
@@ -0,0 +1,75 @@
+using UnityEngine;
+
+public class BaseController : MonoBehaviour
+{
+    protected Rigidbody2D rigidbody2D;
+
+    [SerializeField] private SpriteRenderer _characterRenderer;
+    [SerializeField] private Transform weaponPivot;
+
+    protected Vector2 movementDirection = Vector2.zero;
+    public Vector2 MovementDirection { get { return movementDirection; } }
+
+    protected Vector2 lookDirection = Vector2.zero;
+    public Vector2 LookDirection { get { return lookDirection; } }
+
+    private Vector2 _knockbackDirection = Vector2.zero;
+    private float _knockbackDuration = 0f;
+    private float moveSpeed = 5f;
+
+    protected virtual void Awake()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+
+    }
+
+    protected virtual void Start()
+    {
+
+    }
+
+    protected virtual void Update()
+    {
+        Rotate(lookDirection);
+    }
+
+    protected virtual void FixedUpdate()
+    {
+        Movement(movementDirection);
+        if (_knockbackDuration > 0f) _knockbackDuration -= Time.fixedDeltaTime;
+    }
+
+    private void Movement(Vector2 dir)
+    {
+        if (rigidbody2D == null) return;
+
+        dir = dir * moveSpeed;
+        //Apply Knockback
+        if (_knockbackDuration > 0.0f)
+        {
+            dir *= 0.2f;
+            dir += _knockbackDirection;
+        }
+
+        rigidbody2D.velocity = dir;
+    }
+
+    private void Rotate(Vector2 dir)
+    {
+        float rotationZ = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        bool isLeft = Mathf.Abs(rotationZ) > 90f;
+
+        _characterRenderer.flipX = isLeft;
+
+        if (weaponPivot != null)
+        {
+            weaponPivot.rotation = Quaternion.Euler(0f, 0f, rotationZ);
+        }
+    }
+    public void ApplyKnockback(Transform other, float power, float duration)
+    {
+        _knockbackDuration = duration;
+        _knockbackDirection = -(other.position - _characterRenderer.transform.position).normalized * power;
+    }
+
+}
---
+++ Assets/02.Scripts/Controller/PlayerController.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : BaseController
+{
+    private Camera _camera;
+
+    protected override void Start()
+    {
+        base.Start();
+        _camera = Camera.main;
+    }
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        movementDirection = context.ReadValue<Vector2>();
+        movementDirection = movementDirection.normalized;
+    }
+
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        Vector2 mousePosition = context.ReadValue<Vector2>();
+        
+        Vector2 worldPos = _camera.ScreenToWorldPoint(mousePosition);
+        lookDirection = (worldPos - (Vector2)transform.position);
+        if (lookDirection.magnitude < .9f)
+        {
+            lookDirection = Vector2.zero;
+        }
+        else
+        {
+            lookDirection = lookDirection.normalized;
+        }
+        Debug.Log(lookDirection);
+    }
+}
